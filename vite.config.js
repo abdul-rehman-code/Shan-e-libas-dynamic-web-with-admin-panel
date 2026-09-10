8ccef7ee9f93017419a1a8a08bd0f5f83474270f
unknown <awanm6815@gmail.com>
2026-09-10 20:59:50 +0500

    category design changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -7,7 +8,11 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss(),
     ],
-    // Agar live server par public_html main directory hy:
-    base: process.env.NODE_ENV === 'production' ? '/' : '/',
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
 });
